2d51259aa6a2786f4e1b5f0ca9461cc634f85023
Dick Wissink <dwissink@gmail.com>
2020-05-18 17:48:04 +0200

    motos views updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'motos#index'
   devise_for :users#, only: [:new, :create]
     # resources :motos, except: [:index, :show]
   resources :motos#, only: [:index, :show, :new, :create]

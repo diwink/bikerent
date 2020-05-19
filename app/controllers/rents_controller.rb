25aa698d4c049c68cf3887e4106068e8d4be7398
Dick Wissink <dwissink@gmail.com>
2020-05-19 17:25:05 +0200

    rent form update

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -1,21 +1,30 @@
 class RentsController < ApplicationController
+  # before_action :find_moto, only [ :new ]
 
   def new
+    @moto = Moto.find(params[:moto_id])
     @rent = Rent.new
   end
 
   def create
     @rent = Rent.new(rent_params)
-    @moto = @moto.rent
-    @user = current.user
-    @rent.save
-    redirect_to rent_path
+    @rent.moto = Moto.find(params[:moto_id])
+    @rent.user = current_user
+    if @rent.save
+      redirect_to moto_path(@rent.moto)
+    else
+      render :new
+    end
   end
 
   private
 
   def rent_params
-    params.require(:rent).permit(:start_date, :end_date)
+    params.require(:rent).permit(:moto_id, :start_date, :end_date)
   end
+
+  # def find_moto
+  #   @moto = Moto.find(params[:moto_id])
+  # end
 
 end

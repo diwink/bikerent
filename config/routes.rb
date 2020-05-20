a0b3dfc610ea54005699cc8ad1d3dfb820b0e41f
Dick Wissink <dwissink@gmail.com>
2020-05-20 12:44:22 +0200

    my rent page created + linked in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :motos do
     resources :rents, only: [:new, :create]
   end
+  resources :rents, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

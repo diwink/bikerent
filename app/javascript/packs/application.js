51aef1aa66d852773b5a277245a8f6ef887cb9db
quentin natoly <quentin.natoly@gmail.com>
2020-05-21 13:31:24 +0200

    clickeable card / footer / banner

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,10 +27,8 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-import { clickOnCard } from 'card'
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  clickOnCard()
 });
